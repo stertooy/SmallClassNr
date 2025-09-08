63076cdffbeb1b85fb74946460681a1f1fdec23a
stertooy <5571903+stertooy@users.noreply.github.com>
2025-09-08 23:34:19 +0200

    Update manual.gd

--- doc/manual.gd
+++ doc/manual.gd
@@ -1,4 +1,3 @@
-###############################################################################
 #! @Abstract
 #! The &PACKAGENAME; package package provides access to finite groups with
 #! small class number. Currently, the package contains the finite groups of
@@ -119,11 +118,10 @@ LoadPackage( "SmallClassNr" );
 
 #! @Returns the <A>i</A>-th finite group of class number <A>k</A> in the library.
 #! @Description
-#! Alternatively, the pair <C>[ <A>k</A>, <A>i</A>]</C> can be given as a single argument <A>id</A>.
+#! Alternatively, the pair <C>[ <A>k</A>, <A>i</A> ]</C> can be given as a single argument <A>id</A>.
 #! If the group is solvable, it is given as a PcGroup whose Pcgs is a SpecialPcgs.
 #! If the group is not solvable, it will be given as a permutation group of minimal permutation degree and with a minimal generating set.
 #! @Arguments k, i
-#! @Arguments id
 DeclareGlobalFunction( "SmallClassNrGroup" );
 
 #! @BeginExample
@@ -135,7 +133,6 @@ IsDihedralGroup( G );
 #! true
 #! @EndExample
 
-
 #! @Returns <K>true</K> if the finite groups of class number <A>k</A> are available in the library, and <K>false</K> otherwise.
 #! @Arguments k
 DeclareGlobalFunction( "SmallClassNrGroupsAvailable" );
@@ -146,7 +143,6 @@ SmallClassNrGroupsAvailable( 14 );
 SmallClassNrGroupsAvailable( 15 );
 #! false
 #! @EndExample
-
 
 #! @Returns all finite groups with certain properties as specified by <A>arg</A>.
 #! @Description
@@ -181,8 +177,8 @@ ForAll( L2, G -> IsSolvable( G ) and NrConjugacyClasses( G ) = 6 );
 #! @Returns one finite group with certain properties as specified by <A>arg</A>.
 #! @Description
 #! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <K>NrConjugacyClasses</K>.
-#! Missing functions will be interpreted as <K>NrConjugacyClasses</K>, missing values as <K>true</K>.
+#! At least one of the functions must be <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "OneSmallClassNrGroup" );
 
@@ -200,8 +196,8 @@ NrConjugacyClasses( K ) = 10 and IsSolvable( K ) and not IsNilpotent( K );
 #! @Returns the number of finite groups with certain properties as specified by <A>arg</A>.
 #! @Description
 #! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <K>NrConjugacyClasses</K>.
-#! Missing functions will be interpreted as <K>NrConjugacyClasses</K>, missing values as <K>true</K>.
+#! At least one of the functions must be <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "NrSmallClassNrGroups" );
 
@@ -216,8 +212,8 @@ NrSmallClassNrGroups( IsSolvable, true, NrConjugacyClasses, 6 );
 
 #! @Returns an iterator that iterates over the finite groups with properties as specified by <A>arg</A>.
 #! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <K>NrConjugacyClasses</K>.
-#! Missing functions will be interpreted as <K>NrConjugacyClasses</K>, missing values as <K>true</K>.
+#! At least one of the functions must be <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "IteratorSmallClassNrGroups" );
 #! @EndGroup
@@ -236,8 +232,9 @@ for G in iter do Print( Size( G ), "\n" ); od;
 #! 29120
 #! @EndExample
 
-#! @Returns the <B>SmallClassNr</B> ID of <A>G</A>, i.e. a pair <C>[<A>k</A>, <A>i</A>]</C> such that <A>G</A> is isomorphic to <C>SmallClassNrGroup( <A>k</A>, <A>i</A> )</C>.
-#! @Arguments k
+#! @Returns the &PACKAGENAME; ID of <A>G</A>, i.e. a pair <C>[ <A>k</A>, <A>i</A> ]</C> such that <A>G</A> is isomorphic to <C>SmallClassNrGroup( <A>k</A>, <A>i</A> )</C>.
+#! @Label
+#! @Arguments G
 DeclareAttribute( "IdClassNr", IsGroup );
 
 #! @BeginExample
