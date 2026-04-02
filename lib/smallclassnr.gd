f36f0aac5c5d258304a225cea336f0a415ec20a0
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-02 14:42:49 +0200

    Update smallclassnr.gd

--- lib/smallclassnr.gd
+++ lib/smallclassnr.gd
@@ -27,25 +27,25 @@ DeclareAttribute( "IdClassNr", IsGroup );
 
 #! @Returns all finite groups with certain properties as specified by
 #! <A>arg</A>.
-#! @Arguments arg
+#! @Arguments arg...
 DeclareGlobalFunction( "AllSmallClassNrGroups" );
 #! @InsertChunk example_allsmallclassnrgroups
 
 #! @Returns one finite group with certain properties as specified by
 #! <A>arg</A>.
-#! @Arguments arg
+#! @Arguments arg...
 DeclareGlobalFunction( "OneSmallClassNrGroup" );
 #! @InsertChunk example_onesmallclassnrgroup
 
 #! @Returns the number of finite groups with certain properties as specified by
 #! <A>arg</A>.
-#! @Arguments arg
+#! @Arguments arg...
 DeclareGlobalFunction( "NrSmallClassNrGroups" );
 #! @InsertChunk example_nrsmallclassnrgroups
 
 #! @Returns an iterator that iterates over the finite groups with properties as
 #! specified by <A>arg</A>.
-#! @Arguments arg
+#! @Arguments arg...
 DeclareGlobalFunction( "IteratorSmallClassNrGroups" );
 #! @InsertChunk example_iteratorsmallclassnrgroups
 
