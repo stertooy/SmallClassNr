5b93a46c1e262fea3d34f2acb75f2bb8d1964b7d
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-02 12:08:23 +0200

    Update smallclassnr.gd

--- lib/smallclassnr.gd
+++ lib/smallclassnr.gd
@@ -1,56 +1,75 @@
 #! @Section Functions
 
 #! @Group smallclassnrgroup
-#! @Returns the <A>i</A>-th finite group of class number <A>k</A> in the library.
+#! @Returns the <A>i</A>-th finite group of class number <A>k</A> in the
+#! library.
 #! @Description
-#! Alternatively, the pair <C>[ <A>k</A>, <A>i</A> ]</C> can be given as a single argument <A>id</A>.
-#! If the group is solvable, it is given as a PcGroup whose Pcgs is a SpecialPcgs.
-#! If the group is not solvable, it will be given as a permutation group of minimal permutation degree and with a minimal generating set.
+#! Alternatively, the pair <C>[ <A>k</A>, <A>i</A> ]</C> can be given as a
+#! single argument <A>id</A>.
+#! If the group is solvable, it is given as a PcGroup whose Pcgs is a
+#! SpecialPcgs.
+#! If the group is not solvable, it will be given as a permutation group of
+#! minimal permutation degree and with a minimal generating set.
 #! @Arguments k, i
 DeclareGlobalFunction( "SmallClassNrGroup" );
 
 #! @Group smallclassnrgroupsavailable
-#! @Returns <K>true</K> if the finite groups of class number <A>k</A> are available in the library, and <K>false</K> otherwise.
+#! @Returns <K>true</K> if the finite groups of class number <A>k</A> are
+#! available in the library, and <K>false</K> otherwise.
 #! @Arguments k
 DeclareGlobalFunction( "SmallClassNrGroupsAvailable" );
 
 #! @Group allsmallclassnrgroups
-#! @Returns all finite groups with certain properties as specified by <A>arg</A>.
+#! @Returns all finite groups with certain properties as specified by
+#! <A>arg</A>.
 #! @Description
-#! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <K>NrConjugacyClasses</K>.
-#! Missing functions will be interpreted as <K>NrConjugacyClasses</K>, missing values as <K>true</K>.
+
+#! The arguments must come in pairs consisting of a function and a value (or
+#! list of possible values). At least one of the functions must be
+#! <K>NrConjugacyClasses</K>.
+#! Missing functions will be interpreted as <K>NrConjugacyClasses</K>, missing
+#! values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "AllSmallClassNrGroups" );
 
 #! @Group onesmallclassnrgroup
-#! @Returns one finite group with certain properties as specified by <A>arg</A>.
+#! @Returns one finite group with certain properties as specified by
+#! <A>arg</A>.
 #! @Description
-#! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <C>NrConjugacyClasses</C>.
-#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
+#! The arguments must come in pairs consisting of a function and a value (or
+#! list of possible values). At least one of the functions must be
+#! <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing
+#! values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "OneSmallClassNrGroup" );
 
 #! @Group nrsmallclassnrgroups
-#! @Returns the number of finite groups with certain properties as specified by <A>arg</A>.
+#! @Returns the number of finite groups with certain properties as specified by
+#! <A>arg</A>.
 #! @Description
-#! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <C>NrConjugacyClasses</C>.
-#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
+#! The arguments must come in pairs consisting of a function and a value (or
+#! list of possible values). At least one of the functions must be
+#! <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing
+#! values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "NrSmallClassNrGroups" );
 
 #! @Group iteratorsmallclassnrgroups
-#! @Returns an iterator that iterates over the finite groups with properties as specified by <A>arg</A>.
-#! The arguments must come in pairs consisting of a function and a value (or list of possible values).
-#! At least one of the functions must be <C>NrConjugacyClasses</C>.
-#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing values as <K>true</K>.
+#! @Returns an iterator that iterates over the finite groups with properties as
+#! specified by <A>arg</A>.
+#! The arguments must come in pairs consisting of a function and a value (or
+#! list of possible values). At least one of the functions must be
+#! <C>NrConjugacyClasses</C>.
+#! Missing functions will be interpreted as <C>NrConjugacyClasses</C>, missing
+#! values as <K>true</K>.
 #! @Arguments arg
 DeclareGlobalFunction( "IteratorSmallClassNrGroups" );
 
 #! @Group idclassnr
-#! @Returns the &PACKAGENAME; ID of <A>G</A>, i.e. a pair <C>[ <A>k</A>, <A>i</A> ]</C> such that <A>G</A>
-#! is isomorphic to <C>SmallClassNrGroup( <A>k</A>, <A>i</A> )</C>.
+#! @Returns the &PACKAGENAME; ID of <A>G</A>, i.e. a pair
+#! <C>[ <A>k</A>, <A>i</A> ]</C> such that <A>G</A>#! is isomorphic to 
+#! <C>SmallClassNrGroup( <A>k</A>, <A>i</A> )</C>.
 #! @Arguments G
 DeclareAttribute( "IdClassNr", IsGroup );
