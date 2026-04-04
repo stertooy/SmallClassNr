2e390c1a62fdf1897a892cd312f3ce37592cc4c0
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-04 12:19:04 +0200

    Update and rename smallclassnr.gd to baisc.gd

--- lib/baisc.gd
+++ lib/baisc.gd
@@ -23,32 +23,6 @@ DeclareGlobalFunction( "SmallClassNrGroup" );
 DeclareAttribute( "IdClassNr", IsGroup );
 #! @InsertChunk example_idclassnr
 
-#! @Section groupsbyprops
-
-#! @Returns all finite groups with certain properties as specified by
-#! <A>arg</A>.
-#! @Arguments arg...
-DeclareGlobalFunction( "AllSmallClassNrGroups" );
-#! @InsertChunk example_allsmallclassnrgroups
-
-#! @Returns one finite group with certain properties as specified by
-#! <A>arg</A>.
-#! @Arguments arg...
-DeclareGlobalFunction( "OneSmallClassNrGroup" );
-#! @InsertChunk example_onesmallclassnrgroup
-
-#! @Returns the number of finite groups with certain properties as specified by
-#! <A>arg</A>.
-#! @Arguments arg...
-DeclareGlobalFunction( "NrSmallClassNrGroups" );
-#! @InsertChunk example_nrsmallclassnrgroups
-
-#! @Returns an iterator that iterates over the finite groups with properties as
-#! specified by <A>arg</A>.
-#! @Arguments arg...
-DeclareGlobalFunction( "IteratorSmallClassNrGroups" );
-#! @InsertChunk example_iteratorsmallclassnrgroups
-
 #! @Section availability
 
 #! @Returns <K>true</K> if the finite groups of class number <A>k</A> are
