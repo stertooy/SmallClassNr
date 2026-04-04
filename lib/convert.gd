585a3158262938be01bc220bf2da61cd73e8b903
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-04 18:28:49 +0200

    Update convert.gd

--- lib/convert.gd
+++ lib/convert.gd
@@ -1,13 +1,22 @@
 #! @Chapter otherlibraries
 
-#! @Section otherlibraries
+#! @Section smallgrp
 
-###############################################################################
 #! @Returns [ x, y ] such that SmallGroup( x, y ) is isomorphic to
 #! SmallClassNrGroup( k, i )
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToIdGroup" );
 # @InsertChunk example_smallclassnrgroup
+
+#! @Section perfgrp
+
+#! @Returns [ x, y ] such that PerfectGroup( x, y ) is isomorphic to
+#! SmallClassNrGroup( k, i )
+#! @Arguments k, i
+DeclareGlobalFunction( "IdClassNrToPerfGrp" );
+# @InsertChunk example_smallclassnrgroup
+
+#! @Section primgrp
 
 #! @Returns [ x, y ] such that PrimitiveGroup( x, y ) is isomorphic to
 #! SmallClassNrGroup( k, i )
@@ -15,11 +24,15 @@ DeclareGlobalFunction( "IdClassNrToIdGroup" );
 DeclareGlobalFunction( "IdClassNrToPrimGrp" );
 # @InsertChunk example_smallclassnrgroup
 
+#! @Section transgrp
+
 #! @Returns [ x, y ] such that TransitiveGroup( x, y ) is isomorphic to
 #! SmallClassNrGroup( k, i )
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToTransGrp" );
 # @InsertChunk example_smallclassnrgroup
+
+#! @Section atlasrep
 
 #! @Returns name such that AtlasGroup( name ) is isomorphic to
 #! SmallClassNrGroup( k, i )
