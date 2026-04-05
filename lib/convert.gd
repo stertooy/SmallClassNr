6dc36d2f5bd41f8ee4108ec119945c23396af04f
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-05 12:22:30 +0200

    Update convert.gd

--- lib/convert.gd
+++ lib/convert.gd
@@ -2,43 +2,43 @@
 
 #! @Section smallgrp
 
-#! @Returns a pair of integers <C>[x,y]</C> such that <C>SmallGroup(x,y)</C> is
-#! isomorphic to <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
+#! @Returns a pair of integers <C>[x, y]</C> such that <C>SmallGroup(x, y)</C>
+#! is isomorphic to <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToIdGroup" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section perfgrp
 
-#! @Returns a pair of integers <C>[x,y]</C> such that
-#! <C>PerfectGroup( x, y )</C> is isomorphic to
-#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
+#! @Returns a pair of integers <C>[x, y]</C> such that
+#! <C>PerfectGroup(x, y)</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToPerfGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section primgrp
 
-#! @Returns a pair of integers <C>[x,y]</C> such that
-#! <C>PrimitiveGroup( x, y )</C> is isomorphic to
-#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
+#! @Returns a pair of integers <C>[x, y]</C> such that
+#! <C>PrimitiveGroup(x, y)</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToPrimGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section transgrp
 
-#! @Returns a pair of integers  <C>[x,y]</C> such that
-#! <C>TransitiveGroup( x, y )</C> is isomorphic to
-#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
+#! @Returns a pair of integers <C>[x, y]</C> such that
+#! <C>TransitiveGroup(x, y)</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToTransGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section atlasrep
 
-#! @Returns a string <C>name</C> such that <C>AtlasGroup( name )</C> is
-#! isomorphic to <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
+#! @Returns a string <C>name</C> such that <C>AtlasGroup(name)</C> is
+#! isomorphic to <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToAtlasName" );
 # @InsertChunk example_smallclassnrgroup
