7a5a64d3ccda00dc13f77af2107d658d96b0edf0
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-05 12:09:26 +0200

    Update convert.gd

--- lib/convert.gd
+++ lib/convert.gd
@@ -2,40 +2,40 @@
 
 #! @Section smallgrp
 
-#! @Returns [ x, y ] such that SmallGroup( x, y ) is isomorphic to
-#! SmallClassNrGroup( k, i )
+#! @Returns <C>[x,y]</C> such that <C>SmallGroup(x,y)</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToIdGroup" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section perfgrp
 
-#! @Returns [ x, y ] such that PerfectGroup( x, y ) is isomorphic to
-#! SmallClassNrGroup( k, i )
+#! @Returns <C>[x,y]</C> such that <C>PerfectGroup( x, y )</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToPerfGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section primgrp
 
-#! @Returns [ x, y ] such that PrimitiveGroup( x, y ) is isomorphic to
-#! SmallClassNrGroup( k, i )
+#! @Returns <C>[x,y]</C> such that <C>PrimitiveGroup( x, y )</C> is isomorphic
+#! to <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToPrimGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section transgrp
 
-#! @Returns [ x, y ] such that TransitiveGroup( x, y ) is isomorphic to
-#! SmallClassNrGroup( k, i )
+#! @Returns <C>[x,y]</C> such that <C>TransitiveGroup( x, y )</C> is isomorphic
+#! to <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToTransGrp" );
 # @InsertChunk example_smallclassnrgroup
 
 #! @Section atlasrep
 
-#! @Returns name such that AtlasGroup( name ) is isomorphic to
-#! SmallClassNrGroup( k, i )
+#! @Returns <C>name</C> such that <C>AtlasGroup( name )</C> is isomorphic to
+#! <C>SmallClassNrGroup(<A>k</A>,<A>i</A>)</C>.
 #! @Arguments k, i
 DeclareGlobalFunction( "IdClassNrToAtlasName" );
 # @InsertChunk example_smallclassnrgroup
