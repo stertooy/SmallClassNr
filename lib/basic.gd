8266cd99f54cfbe0ed4b19b97a7c3e44aca039d2
stertooy <5571903+stertooy@users.noreply.github.com>
2026-04-05 12:23:23 +0200

    Update basic.gd

--- lib/basic.gd
+++ lib/basic.gd
@@ -5,7 +5,7 @@
 #! @Returns the <A>i</A>-th finite group of class number <A>k</A> in the
 #! library.
 #! @Description
-#! Alternatively, the pair <C>[ <A>k</A>, <A>i</A> ]</C> can be given as a
+#! Alternatively, the pair <C>[<A>k</A>, <A>i</A>]</C> can be given as a
 #! single argument <A>id</A>.
 #! If the group is solvable, it is given as a PcGroup whose Pcgs is a
 #! SpecialPcgs.
@@ -16,8 +16,8 @@ DeclareGlobalFunction( "SmallClassNrGroup" );
 #! @InsertChunk example_smallclassnrgroup
 
 #! @Returns the &PACKAGENAME; ID of <A>G</A>, i.e. a pair
-#! <C>[ <A>k</A>, <A>i</A> ]</C> such that <A>G</A> is isomorphic to 
-#! <C>SmallClassNrGroup( <A>k</A>, <A>i</A> )</C>.
+#! <C>[<A>k</A>, <A>i</A>]</C> such that <A>G</A> is isomorphic to 
+#! <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Label
 #! @Arguments G
 DeclareAttribute( "IdClassNr", IsGroup );
