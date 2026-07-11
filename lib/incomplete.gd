196491a7002f219ef0ab969dab87d9e6cadba57d
stertooy <5571903+stertooy@users.noreply.github.com>
2026-07-11 22:51:40 +0200

    Create incomplete.gd

---
+++ lib/incomplete.gd
@@ -0,0 +1,7 @@
+#! @Chapter classification
+
+@Section incompleteclass
+
+#! @Description Loads the known groups with class number 15 to 20.
+#! @Arguments
+DeclareGlobalFunction( "LoadIncompleteSmallClassNrGroups" );
