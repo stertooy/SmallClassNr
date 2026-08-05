12fec95e5d0823efd1fa43852c20e5448611748f
stertooy <5571903+stertooy@users.noreply.github.com>
2026-08-05 23:57:22 +0200

    Cleanup whitespace

--- lib/basic.gd
+++ lib/basic.gd
@@ -20,7 +20,7 @@ DeclareGlobalName( "SmallClassNrGroup" );
 #! @InsertChunk example_smallclassnrgroup
 
 #! @Returns the &PACKAGENAME; ID of <A>G</A>, i.e. a pair
-#! <C>[<A>k</A>, <A>i</A>]</C> such that <A>G</A> is isomorphic to 
+#! <C>[<A>k</A>, <A>i</A>]</C> such that <A>G</A> is isomorphic to
 #! <C>SmallClassNrGroup(<A>k</A>, <A>i</A>)</C>.
 #! @Label
 #! @Arguments G
